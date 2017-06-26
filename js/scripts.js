29f51f1d5eeab2c641252bebaeae52c4ec60c179
Sammie123 <huylincoln007@yahoo.com>
2017-06-25 23:50:21 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,23 @@
+function pP (userNum) {
+  for (var index = 1; index <= userNum; index +=1 ) {
+    if ((index % 3 === 0) && (index % 5 === 0)) {
+      $("ul#results").append('<li>' + "pingpong" + '</li>');
+    } else if (index % 5 === 0) {
+      $("ul#results").append('<li>' + "pong" + '</li>');
+    } else if (index % 3 === 0) {
+      $("ul#results").append('<li>' + "ping" + '</li>');
+    } else {
+      $("ul#results").append('<li>' + index + '</li');
+    };
+  };
+};
+
+
 $(document).ready(function() {
   $("form#pingPong").submit(function(event) {
-    var userNum = parseInt($("input#userInput").val());
-    for (index = 1; index <= userNum; index +=1 ) {
-      if ((index % 3 === 0) && (index % 5 === 0)) {
-        $("ul#results").append('<li>' + "pingpong" + '</li>');
-      } else
-          if (index % 5 === 0) {
-            $("ul#results").append('<li>' + "pong" + '</li>');
-          } else if (index % 3 === 0) {
-            $("ul#results").append('<li>' + "ping" + '</li>');
-          } else {
-            $("ul#results").append('<li>' + index + '</li>');
-          };
-        };
-    $("#results").show();
     event.preventDefault();
+    var userNum = parseInt($("input#userInput").val());
+    $("#results").show();
+    pP (userNum);
   });
 });
